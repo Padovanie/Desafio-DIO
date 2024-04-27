5f2f8606c4bb547ea6068fba7df80f09bf85795d
Emelyn <emelyn_p@hotmail.com>
2024-04-27 17:21:58 -0300

    Realização do desafio proposto pela DIO - conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        int numeroConta;
+        String agencia, nomeCliente;
+        float saldo;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da conta:");
+        numeroConta = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da agência:");
+        agencia = sc.next();
+
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o saldo:");
+        saldo = sc.nextFloat();
+        sc.close();
+
+        System.out.println("Olá " + nomeCliente + ", sua conta foi criada com sucesso! Obrigado por escolher nosso banco!\nSua agência é: " + agencia + ", conta: " + numeroConta + " e seu saldo de " +  saldo + " já está disponível para saque!");
+
     }
 }
